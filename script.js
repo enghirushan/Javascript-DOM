66721a9071eb2f7a9f7feb08adac73e47b4b38ed
enghirushan <eng.hirushan@gmail.com>
2024-06-21 11:12:02 +0530

    add querySelector and All

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ heading.style.fontFamily="arial";
 
 const section =document.getElementsByClassName("section");
  for(let i=0;i<section.length;i++){
-    section[i].style.color="blue";
+    section[i].style.color="#18182d";
     section[i].style.fontFamily="poppins";
     section[i].style.fontSize="25px";
 
@@ -29,3 +29,39 @@ const section =document.getElementsByClassName("section");
 
  }
   
+
+ //querySelector===>not array Type
+
+ const bodycontent =document.querySelector("body");
+ bodycontent.style.backgroundColor = "#39736e";
+
+ const items = document.querySelector(".items")
+ items.innerHTML +="<li> new list 6</li><li>new list 7</li>"
+
+//we cannot use below for loop.because this not array type
+ // for(let i=0;i<items.length;i++){
+//    console.log(items);
+// }
+
+const gallerimgcont =document.querySelector(".gallerimgcont");
+gallerimgcont.style.display ="flex";
+gallerimgcont.style.flexWrap="wrap";
+
+
+
+ //querySelectorAll===>array Type
+
+ const gallerimg =document.querySelectorAll(".gallerimg");
+ gallerimg.forEach(eb=>{
+    eb.style.maxWidth = "50%";
+
+ })
+
+const ant = document.querySelectorAll(".items li")
+ant.forEach(nm=>{
+   nm.style.fontFamily="poppins";
+})
+
+// for(let i=0;i<ant.length;i++){
+//    ant[i].style.fontFamily="popins";
+// }
